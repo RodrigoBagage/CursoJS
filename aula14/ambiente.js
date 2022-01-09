62c5091035be24484c3792218d001a6ab5bb5ec6
Rodrigo Queiroz <rodrigojosequeiroz@gmail.com>
2022-01-09 09:02:03 -0300

    Aula13 e 14

---
+++ aula14/ambiente.js
@@ -0,0 +1,19 @@
+console.log('Vai comecar...')
+for(var c = 1;c <= 4;c++){
+    console.log(c)
+}
+console.log('FIM!')
+
+/*
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+*/
+//------------------
+/*
+for(var c = 1;c <= 6;c++){
+    console.log(c)
+}
+*/
